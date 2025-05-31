3eeecdf8c93b96875eb680da876cb3f2ba75dba2
SanjayDevarajan03 <sanjay.devarajan02@gmail.com>
2025-05-31 00:51:48 -0400

    Updated embedding.py

--- model/attention.py
+++ model/attention.py
@@ -1,10 +1,16 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 class ScaledAttention(nn.Module):
-    def __init__(self, query, key, value, vocab_size):
+    def __init__(self, query, key, value, vocab_size, d_model):
         super().__init__()
-        query = nn.Embedding(vocab_size, )
+        query = nn.Embedding(vocab_size, d_model)
+        key = nn.Embedding(vocab_size, d_model)
+        value = nn.Embedding(vocab_size, d_model)
+
+        attention = nn.Softmax((query*torch.transpose(key))/np.sqrt(len(key)))* value
+
 
 
 class MultiHeadAttention:
@@ -13,6 +19,13 @@ class MultiHeadAttention:
         self.query_embedding = nn.Embedding(vocab_size, len(key))
         self.key_embedding = nn.Embedding(vocab_size, len(key))
         self.value_embedding = nn.Embedding(vocab_size, len(value))
+
+    def forward(self):
+        self.net = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(28*28, 128),
+            nn.ReLU()
+        )
 
 
     
--- model/embedding.py
+++ model/embedding.py
@@ -4,24 +4,23 @@ import math
 import numpy as np
 from typing import Optional, Tuple
 
-
-class PositionalEmbedding:
-    def __init__(self, vocab_size, d_model, max_len = 500):
+class PositionalEncoding(nn.Module):
+    def __init__(self, vocab_size,d_model,max_len=500):
         super().__init__()
         self.token_embedding = nn.Embedding(vocab_size, d_model)
         self.d_model = d_model
 
-        # Positional encoding matrix (1, max_len, d_model)
+        # Positional encoding matrix (1, max_len, d_model) 
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len).unsqueeze(1).float()
-        div_term = torch.exp(torch.arange(0, d_model,2 ).float() * (-math.log(10000.0) / d_model))
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
 
-        self.token_embedding[:, 0::2] = torch.sin(position * div_term)
-        self.token_embedding[:, 1::2] = torch.cos(position * div_term)
+        self.token_embedding[:, 0::2] = torch.sin(position*div_term)
+        self.token_embedding[:, 1::2] = torch.cos(position*div_term)
         pe = pe.unsqueeze(0)
 
         self.register_buffer('pe', pe)
-
+    
     def forward(self, x):
         # x: (batch_size, seq_len)
         seq_len = x.size(1)
