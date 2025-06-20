8bbff31592188df99c0c97add9064f8645b75486
SanjayDevarajan03 <sanjay.devarajan02@gmail.com>
2025-06-19 22:53:08 -0400

    Added generate.py

--- generate.py
+++ generate.py
@@ -2,5 +2,22 @@ import torch
 import torch.nn as nn
 from torchtyping import TensorType
 
-def generate():
-    pass
+def generate(model,new_chars:int, context: TensorType[int], context_length:int, int_to_char: int) -> str:
+    generator = torch.manual_seed(0)
+    initial_state = generator.get_state()
+    res = []
+    # context = B X T
+    for i in range(new_chars):
+        if len(context.T) > context_length:
+            context = context[:, -context_length:]
+        prediction = model(context) # B,T, V
+        last_time_step = prediction[:, -1, :] # B, V
+        probs = nn.functional.softmax(last_time_step, dim=-1)
+        next_char = torch.multinomial(probs, 1, generator=generator)
+        generator.get_state(initial_state)
+        context = torch.cat((context, next_char), dim=-1) # B, T -> B, T + 1
+        int_to_char[next_char.item()]
+        res.append(int_to_char[next_char.item()])
+        
+    return ''.join(res)
+    
--- model/encoder.py
+++ model/encoder.py
@@ -7,16 +7,31 @@ from NeuralNetwork import NeuralNetwork
 from torchtyping import TensorType
     
 class Bert(nn.Module):
-    def __init__(self):
+    def __init__(self, model_dim: int, context_length: int, vocab_size: int, num_blocks: int, num_heads:int):
         super().__init__()
         torch.manual_seed(0)
+        self.token_embedding = nn.Embedding(vocab_size, model_dim)
+        self.positional_embedding = nn.Embedding(context_length, model_dim)
+        self.transformer_block = nn.Sequential()
+        self.layer_norm = nn.LayerNorm(model_dim)
+        for i in range(num_blocks):
+            self.transformer_block.append(self.TransformerBlock(model_dim, num_heads))
+        self.vocab_projection = nn.Linear(model_dim, vocab_size)
+
 
     
-    def forward(self,embedded: TensorType[float]) -> TensorType[float]:
+    def forward(self,context: TensorType[int]) -> TensorType[float]:
         torch.manual_seed(0)
+        embedded = self.token_embedding(context)
+        batch_size, context_length = context.shape
+        positions = torch.arange(context_length, device=context.device).unsqueeze(0).expand(batch_size, -1)
+        embedded = embedded + self.positional_embedding(positions)
+        raw_output = self.vocab_projection(self.layer_norm(self.transformer_block(embedded)))
+        probabilities = nn.functional.softmax(raw_output, dim=-1)
+        return torch.round(probabilities, decimals=4)
+    
 
-
-    class TransformerBlack(nn.Module):
+    class TransformerBlock(nn.Module):
 
         class MultiHeadAttention(nn.Module):
 
@@ -56,7 +71,6 @@ class Bert(nn.Module):
                 head_outputs = []
                 for head in self.att_heads:
                     head_outputs.append(head(embedded))
-
                 concatenated = torch.cat(head_outputs, dim=2)
                 return torch.round(concatenated, decimals=4)
             
@@ -67,15 +81,25 @@ class Bert(nn.Module):
                 self.up_projection = nn.Linear(model_dim, model_dim*4)
                 self.relu = nn.ReLU()
                 self.down_projection = nn.Linear(model_dim*4, model_dim)
-                self.dropout = nn.Dropout(p=0.2)
+                self.dropout = nn.Dropout(0.2)
             
             def forward(self, embedded: TensorType[float]) -> TensorType[float]:
                 torch.manual_seed(0)
-                return self.dropout(self.sigmoid(self.last_layer(self.first_layer(embedded))))
+                return self.dropout(self.down_projection(self.relu(self.up_projection(embedded))))
+
             
         def __init__(self, model_dim: int, num_heads: int):
             super().__init__()
             torch.manual_seed(0)
+            self.attention = self.MultiHeadAttention(model_dim, num_heads)
+            self.layer_network = self.NeuralNetwork(model_dim)
+            self.first_norm = nn.LayerNorm(model_dim)
+            self.second_norm = nn.LayerNorm(model_dim)
 
-        def forward(self,):
-            pass
+        def forward(self, embedded: TensorType[float]) -> TensorType[float]:
+            torch.manual_seed(0)
+            embedded = embedded + self.attention(self.first_norm(embedded))
+            embedded = embedded + self.layer_network(self.second_norm(embedded))
+            return torch.round(embedded, decimals=4)
+
+    
