4b01af86f0ecd6426fe467b89b47575c8e8d28bb
SanjayDevarajan03 <sanjay.devarajan02@gmail.com>
2025-07-01 11:21:00 -0400

    Updated attention.py

--- model/attention.py
+++ model/attention.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 
-class MultiHeadAttention:
+class MultiHeadAttention(nn.Module):
     def __init__(self, embedding_dim: int, attention_dim: int, num_heads: int):
         super().__init__()
         torch.manual_seed(0)
@@ -23,8 +23,30 @@ class MultiHeadAttention:
         concatenated = torch.cat(head_outputs, dim=2)
         return torch.round(concatenated, decimals=4)
 
-    class SingleHeadAttention:
+    class SingleHeadAttention(nn.Module):
         def __init__(self, embedding_dim, attention_dim):
+            super().__init__()
+            torch.manual_seed(0)
+            self.key = nn.Linear(embedding_dim, attention_dim, bias=False)
+            self.value = nn.Linear(embedding_dim, attention_dim, bias=False)
+            self.query = nn.Linear(embedding_dim, attention_dim, bias=False)
+
+        def forward(self, embedded: TensorType[float]) -> TensorType[float]:
+            key = self.key(embedded)
+            value = self.value(embedded)
+            query = self.query(embedded)
+
+            scores = torch.matmul(query, torch.transpose(key,1,2))
+            context_length, attention_dim = key.shape[1], key.shape[2]
+            scores = scores/(attention_dim**0.5)
+
+            lower_triangular = torch.tril(torch.ones(context_length, context_length))
+            mask = lower_triangular == 0
+            scores = scores.masked_fill(mask, float('-inf'))
+
+            scores = nn.functional.softmax(scores,dim=2)
+            return torch.round(torch.matmul(scores, value), decimals=4)
+        """def __init__(self, embedding_dim, attention_dim):
             super().__init__()
             torch.manual_seed(0)
 
@@ -46,6 +68,6 @@ class MultiHeadAttention:
             scores = scores.masked_fill(mask, float('-inf'))
             scores = nn.Softmax(dim=2)(scores)
 
-            return torch.round(torch.matmul(scores, value), decimals=4)
+            return torch.round(torch.matmul(scores, value), decimals=4)"""
 
 
